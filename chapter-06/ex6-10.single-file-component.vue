db728e0bd67cc7fff86cd7f5050eeece9499ca35
freebz <>
2020-08-31 10:46:23 +0900

    6장 컴포넌트 사용

---
+++ chapter-06/ex6-10.single-file-component.vue
@@ -0,0 +1,22 @@
+<!-- 코드 6-10 단일 파일 컴포넌트: chapter-06/single-file-component.vue -->
+
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Vue.js 앱에 오신 걸 환영합니다'
+    }
+  }
+}
+</script>
+
+<!-- CSS 범위 지정을 위해 해당 컴포넌트에만 "scoped" 속성을 추가하세요. -->
+<style scoped>
+</style>
